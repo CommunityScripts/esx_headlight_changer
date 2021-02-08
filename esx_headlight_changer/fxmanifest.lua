5ca0c0f28b54f91df26b034406c68c69b7e00215
CommunityScripts <78282590+CommunityScripts@users.noreply.github.com>
2021-02-08 19:37:49 +0100

    Delete esx_headlight_changer directory

--- esx_headlight_changer/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'adamant'
-games { 'rdr3', 'gta5' }
-
-description 'Headlight changer edited by bypass#1877'
-
-client_scripts {
-    'client/headlights_cl.lua',
-    'client/functions.lua',
-}
