4145af5c30b81911f1353b7b835e250e2ad49766
CommunityScripts <78282590+CommunityScripts@users.noreply.github.com>
2021-02-08 19:37:15 +0100

    Add files via upload

---
+++ esx_headlight_changer/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+
+description 'Headlight changer edited by bypass#1877'
+
+client_scripts {
+    'client/headlights_cl.lua',
+    'client/functions.lua',
+}
