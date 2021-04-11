2640818861b6b3f17115dd1d67573622388fe083
Vvs's Development <78282590+vvs0700@users.noreply.github.com>
2021-04-11 20:07:56 +0200

    Update headlights_cl.lua

--- esx_headlight_changer/client/headlights_cl.lua
+++ esx_headlight_changer/client/headlights_cl.lua
@@ -1,9 +1,6 @@
 ESX = nil
 
-Citizen.CreateThread(function()
-    while ESX == nil do
-	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-	Citizen.Wait(0)
-    end
+CreateThread(function()
+    while ESX == nil do TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) Wait(0) end
 end)
 
